35960a9db6dfe3dcaec6e32e33ef446a9a63fce7
Josh Wallace <josh.wallace2@washpost.com>
2023-09-08 12:45:55 -0400

    add example form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [isVisible, setIsVisible] = useState(false);
+
+  function handleSumbit(e) {
+    e.preventDefault();
+    setIsVisible(true);
+  }
+
+  function handleOnChange(e) {
+    setUsername(e.target.value);
+  }
+
+  function handleClear() {
+    setUsername("");
+    setIsVisible(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is my RTL Demo app</h1>
+      <form onSubmit={handleSumbit}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={handleOnChange}
+        />
+        <button type="submit">Submit</button>
+        <button type="button" onClick={handleClear}>
+          Clear
+        </button>
+      </form>
+      <h2>Your username is: {isVisible && username}</h2>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("renders an H1 tag with correct text", () => {
+    render(<App />);
+    const linkElement = screen.getByRole("heading", {
+      level: 1,
+      name: /This is my RTL Demo app/i,
+    });
+    expect(linkElement).toBeInTheDocument();
+  });
 });
